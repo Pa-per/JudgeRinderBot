164b075b24e744de9377b9a6a7cb0e0b0ff48985
Pa-per <69976499+Pa-per@users.noreply.github.com>
2022-05-15 20:10:41 +0100

    🐞 fix: checks.py False
    cmd_channel was always returning False, fixed

--- utils/checks.py
+++ utils/checks.py
@@ -1,6 +1,6 @@
 def is_cmd_channel(ctx):
-    if ctx.channel.id == 973399767260471358:
+    if ctx.channel.id == 973583752477433896:
         return True
-    if ctx.channel.id != 973583752477433896:
-        return False
+    if ctx.channel.id == 973421932546519090:
+        return True
     return False

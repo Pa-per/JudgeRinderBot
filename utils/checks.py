af306b0cbcc8d1bd9af64e0047f0646bb573dbc6
Pa-per <69976499+Pa-per@users.noreply.github.com>
2022-05-15 19:05:48 +0100

    🐞 fix: checks.py
    Redundant else

--- utils/checks.py
+++ utils/checks.py
@@ -1,5 +1,6 @@
 def is_cmd_channel(ctx):
     if ctx.channel.id == 973399767260471358:
         return True
-    elif ctx.channel.id != 973583752477433896:
+    if ctx.channel.id != 973583752477433896:
         return False
+    return False

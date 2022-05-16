3edf71b50f12e564fa5804edc3b266998c50b031
Pa-per <69976499+Pa-per@users.noreply.github.com>
2022-05-16 20:38:32 +0100

    Update on_message.py

--- events/on_message.py
+++ events/on_message.py
@@ -1,3 +1,6 @@
+"""Event listener functions for the bot."""
+
+
 import random
 import time
 
@@ -7,13 +10,30 @@ from utils.db import add_exp, create_profile, get_level
 
 
 class OnMessage(discord.ext.commands.Cog):
+    """Class to specify that this class is a cog."""
+
     def __init__(self, client):
+        """
+        __init__ Initialise the class.
+
+        Args:
+            client: The client the bot is using.
+        """
         self.client = client
         self.xp_cooldown = 0
         self.cooldown_members = {}
 
     @discord.ext.commands.Cog.listener()
     async def on_message(self, message):
+        """
+        on_message The event the bot will listen to.
+
+        Args:
+            message: The message the bot will use to gather data from.
+        """
+        member_cooldown = self.cooldown_members.setdefault(
+            message.author.id, 0,
+        )
         if message.author.bot:
             return
         if message.guild is None:
@@ -21,7 +41,6 @@ class OnMessage(discord.ext.commands.Cog):
         check = await self.client.get_context(message)
         if check.valid:
             return
-        member_cooldown = self.cooldown_members.setdefault(message.author.id, 0)
 
         if time.time() - member_cooldown < self.xp_cooldown:
             return
@@ -30,11 +49,18 @@ class OnMessage(discord.ext.commands.Cog):
         levelled = await add_exp(message.author.id, message_xp)
         self.cooldown_members[message.author.id] = time.time()
         if levelled:
-            channel = self.client.get_channel(973399767260471358)
+            level_up_channel_id = 973399767260471358
+            channel = self.client.get_channel(level_up_channel_id)
             level = await get_level(message.author.id)
-            message = f"{message.author.mention} has reached level **{level}**!"
-            await channel.send(message)
+            msg = f'{message.author.mention} has reached level **{level}**!'
+            await channel.send(msg)
 
 
 async def setup(client: discord.ext.commands.Bot):
+    """
+    Create a new cog instance within the client.
+
+    Args:
+        client: the discord client
+    """
     await client.add_cog(OnMessage(client))

674a7edba5cc20b43d0f0c962ca468a34d17a89c
Pa-per <69976499+Pa-per@users.noreply.github.com>
2022-05-16 20:24:08 +0100

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,6 +1,15 @@
+"""Simple functions to act as irregular checks."""
+
+
 def is_cmd_channel(ctx):
-    if ctx.channel.id == 973583752477433896:
-        return True
-    if ctx.channel.id == 973421932546519090:
-        return True
-    return False
+    """
+    Return True if the channel is a command channel.
+
+    Args:
+        ctx: the context the command was called in.
+
+    Returns:
+        True or False depending on if the channel is a command channel.
+    """
+    channel_ids = [973583752477433896, 973421932546519090]
+    return ctx.channel.id in channel_ids
